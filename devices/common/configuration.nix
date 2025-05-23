74477267a8d159351b1f652fe9bd444b0b28592f
MinimalMio <yukati2024@outlook.com>
2025-05-24 02:09:37 +0800

    pre: fxxk nvidia

--- devices/common/configuration.nix
+++ devices/common/configuration.nix
@@ -4,7 +4,7 @@
   nix.settings.experimental-features = [ "nix-command flakes" ];
 
   environment.systemPackages = with pkgs; [
-    vim wget git fish strace binutils gcc gnumake htop
+    vim wget git fish strace binutils gcc gnumake htop lshw
   ];
 
   fonts = {
