a317ebb8f58b55fdfec90bec40430bdd6079f1df
MinimalMio <yukati2024@outlook.com>
2025-05-25 03:46:25 +0800

    add 2 packages

--- devices/nix-on-droid/configuration.nix
+++ devices/nix-on-droid/configuration.nix
@@ -6,6 +6,7 @@
     tzdata hostname man gnugrep gnupg gnused gnutar
     bzip2 gzip xz zip unzip ncurses strace gcc fish
     hyfetch openssh tree clang-tools binutils gnumake
+    sqlite3 gdb
   ];
 
   environment.etcBackupExtension = ".bak";
