61eeab500edbca69c6314e37419662b743c53b72
MinimalMio <yukati2024@outlook.com>
2025-05-25 03:01:26 +0800

    nvim: bufferline: fallback compatiblity changes

--- home-manager/components/nixvim/plugins.nix
+++ home-manager/components/nixvim/plugins.nix
@@ -8,12 +8,8 @@
       nvim-autopairs.enable = true;
       rainbow-delimiters.enable = true;
       treesitter.enable = true;
-      web-devicons.enable = true;
       bufferline = {
         enable = true;
-        # Fallback changes:
-        # Compatible with nix-on-droid (now version 24.05)
-        # it dont support settings.options.offsets .
         offsets = [
           {
             filetype = "NvimTree";
