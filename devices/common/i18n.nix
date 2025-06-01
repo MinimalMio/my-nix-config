5a43d9eac149222e36fe2ac257d11849d9dc9ba0
MinimalMio <yukati2024@outlook.com>
2025-06-01 16:41:06 +0800

    update i18n

--- devices/common/i18n.nix
+++ devices/common/i18n.nix
@@ -4,8 +4,25 @@
   i18n.inputMethod = {
     type = "fcitx5";
     enable = true;
-    fcitx5.addons = with pkgs; [
-      fcitx5-gtk fcitx5-chinese-addons fcitx5-nord
-    ];
+    fcitx5 = {
+      # plasma6Support = true;
+      # waylandFrontend = true;
+      addons = with pkgs; [
+        fcitx5-gtk
+        fcitx5-qt
+        fcitx5-chinese-addons
+        fcitx5-nord
+      ];
+      settings = {
+        inputMethod = {
+          HotKey = "Control+space";
+        };
+        addons = {
+          pinyin = {
+            EnableCloudPinyin = true;
+          };
+        };
+      };
+    };
   };
 }
--- devices/nix-on-droid/packages.nix
+++ devices/nix-on-droid/packages.nix
@@ -6,6 +6,6 @@
     tzdata hostname man gnugrep gnupg gnused gnutar
     bzip2 gzip xz zip unzip ncurses strace gcc fish
     hyfetch openssh tree clang-tools binutils gnumake
-    sqlite gdb cmake zulu8 inetutils
+    sqlite gdb cmake zulu8 inetutils qemu
   ];
 }
