d2d0b5ae92c77ca2a23144525f02b2861ab810c0
MinimalMio <yukati2024@outlook.com>
2025-06-13 03:11:12 +0800

    devices/ 里这么多软件包里翻不出一个 bison

--- devices/nix-on-droid/packages.nix
+++ devices/nix-on-droid/packages.nix
@@ -6,6 +6,7 @@
     tzdata hostname man gnugrep gnupg gnused gnutar
     bzip2 gzip xz zip unzip ncurses strace gcc fish
     hyfetch openssh tree clang-tools binutils gnumake
-    sqlite gdb cmake zulu8 inetutils qemu
+    sqlite gdb cmake zulu8 inetutils qemu cachix
+    bison
   ];
 }
